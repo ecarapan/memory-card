a77cc227413200207af99f18076facd4b55766a8
ecarapan <92954426+ecarapan@users.noreply.github.com>
2025-10-26 22:16:10 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/App.module.css";
+import { useState } from "react";
+
+export default function App() {
+
+  
+  return (
+    <div className={styles.app}>
+
+    </div>
+  );
+}
